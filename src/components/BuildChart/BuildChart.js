5cae9e87dc96e290a86b1c165e7a3fa44dde746b
Hector Ramos <hramos@fb.com>
2018-06-22 12:44:01 -0700

    Update status board to be more useful for RN

--- src/components/BuildChart/BuildChart.js
+++ src/components/BuildChart/BuildChart.js
@@ -1,14 +1,13 @@
-import {
-  CircleLifecycles,
-  TravisStates
- }           from '../../Constants';
-import React from 'react';
-import './BuildChart.css';
+import { CircleLifecycles, TravisStates } from "../../Constants";
+import React from "react";
+import "./BuildChart.css";
 
 class BuildChart extends React.Component {
   _renderBuild(build) {
-    if (build.lifecycle !== CircleLifecycles.FINISHED ||
-      build.status === TravisStates.CREATED) {
+    if (
+      build.lifecycle !== CircleLifecycles.FINISHED ||
+      build.status === TravisStates.CREATED
+    ) {
       return null;
     }
 
@@ -16,35 +15,38 @@ class BuildChart extends React.Component {
       <a
         key={build.build_num}
         target="_blank"
-        data-tooltip={build.subject}
+        data-tooltip={
+          build.workflows ? build.workflows.job_name : build.subject
+        }
         data-status=""
         className={"Test " + build.status}
         href={build.build_url}
       >
+        {build.workflows ? build.workflows.job_name : build.subject}
       </a>
     );
   }
 
   render() {
     return (
-      <div className="builds">
-        {this.props.builds.map(this._renderBuild)}
-      </div>
+      <div className="builds">{this.props.builds.map(this._renderBuild)}</div>
     );
   }
 }
 
 BuildChart.propTypes = {
-  builds: React.PropTypes.arrayOf(React.PropTypes.shape({
-    build_num: React.PropTypes.oneOfType([
-      React.PropTypes.string,
-      React.PropTypes.number,
-    ]),
-    build_url: React.PropTypes.string,
-    lifecycle: React.PropTypes.string,
-    status: React.PropTypes.string,
-    subject: React.PropTypes.string
-  })),
+  builds: React.PropTypes.arrayOf(
+    React.PropTypes.shape({
+      build_num: React.PropTypes.oneOfType([
+        React.PropTypes.string,
+        React.PropTypes.number
+      ]),
+      build_url: React.PropTypes.string,
+      lifecycle: React.PropTypes.string,
+      status: React.PropTypes.string,
+      subject: React.PropTypes.string
+    })
+  )
 };
 
 export default BuildChart;
--- src/components/CircleStatus/CircleStatus.js
+++ src/components/CircleStatus/CircleStatus.js
@@ -1,13 +1,10 @@
-import {
-  RefreshIntervals,
-  RepositoryInfo,
-} from '../../Constants';
-import React from 'react';
-import StatusBadge from '../StatusBadge/StatusBadge';
-import BuildChart from '../BuildChart/BuildChart';
-import 'whatwg-fetch';
-import './CircleStatus.css';
-import Spinner from 'react-spinkit';
+import { RefreshIntervals, RepositoryInfo } from "../../Constants";
+import React from "react";
+import StatusBadge from "../StatusBadge/StatusBadge";
+import BuildChart from "../BuildChart/BuildChart";
+import "whatwg-fetch";
+import "./CircleStatus.css";
+import Spinner from "react-spinkit";
 
 class CircleStatus extends React.Component {
   constructor(props) {
@@ -15,16 +12,13 @@ class CircleStatus extends React.Component {
     this.state = {
       builds: [],
       date: new Date(),
-      isLoading: true,
+      isLoading: true
     };
   }
 
   componentDidMount() {
     this.tick();
-    this.timerID = setInterval(
-      () => this.tick(),
-      RefreshIntervals.STATUS
-    );
+    this.timerID = setInterval(() => this.tick(), RefreshIntervals.STATUS);
   }
 
   componentWillUnmount() {
@@ -38,36 +32,59 @@ class CircleStatus extends React.Component {
   _fetchBuilds() {
     this.setState({ isLoading: true });
 
-    const builds_url = `https://circleci.com/api/v1.1/project/github/${RepositoryInfo.GITHUB_ORG}/${RepositoryInfo.REPO}/tree/master?limit=14`; // use 14 for parity with Travis
+    const builds_url = `https://circleci.com/api/v1.1/project/github/${
+      RepositoryInfo.GITHUB_ORG
+    }/${RepositoryInfo.REPO}/tree/master?limit=6`; // 6 is the current number of jobs in the `tests` workflow
 
     fetch(builds_url, {
       headers: {
-        Accept: 'application/json',
-      },
+        Accept: "application/json"
+      }
     })
       .then(response => response.json())
       .then(json => json.reverse())
-      .then(builds => {
+      .then(recentBuilds => {
+        let builds = recentBuilds.filter(build => {
+          if (build.workflows) {
+            console.log(build.workflows);
+            if (build.workflows.workflow_name === "tests") {
+              return true;
+            }
+
+            return false;
+          } else {
+            return true;
+          }
+        });
         this.setState({
           builds,
           date: new Date(),
           isLoading: false,
+          latestBuild: builds[builds.length - 1]
         });
         this.forceUpdate();
       })
       .catch(ex => {
-        console.log('Unable to obtain builds', ex);
+        console.log("Unable to obtain builds", ex);
       });
   }
 
   render() {
-    const loadingIndicator = this.state.isLoading
-      ? <Spinner spinnerName="double-bounce" noFadeIn />
-      : '';
-    const badgeUrl = `https://circleci.com/gh/${RepositoryInfo.GITHUB_ORG}/${RepositoryInfo.REPO}.svg?style=shield`;
-    const statusUrl = `http://circleci.com/gh/${RepositoryInfo.GITHUB_ORG}/${RepositoryInfo.REPO}/tree/master`;
+    const loadingIndicator = this.state.isLoading ? (
+      <Spinner spinnerName="double-bounce" noFadeIn />
+    ) : (
+      ""
+    );
+    const badgeUrl = `https://circleci.com/gh/${RepositoryInfo.GITHUB_ORG}/${
+      RepositoryInfo.REPO
+    }.svg?style=shield`;
+    const statusUrl = `http://circleci.com/gh/${RepositoryInfo.GITHUB_ORG}/${
+      RepositoryInfo.REPO
+    }/tree/master`;
     return (
       <div className="status">
+        Latest build:{" "}
+        {this.state.latestBuild ? this.state.latestBuild.build_num : "none"}
         <StatusBadge
           name="Circle"
           badgeUrl={badgeUrl}
