eab85faadec8605411d4e6b9a5b4d433097412b4
Hector Ramos <hramos@fb.com>
2018-06-22 12:43:04 -0700

    Use env vars to control badges

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-
 # Open Source CI Status page
 
 Barebones status board for tracking a given project's CI status. Defaults to the react-native repo.
+
+## Usage
+
+Run the following to display the default repo's Circle CI status:
+
+`REACT_APP_ENABLE_CIRCLE=true npm start`
--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import { RefreshIntervals } from './Constants';
-import React from 'react';
-import CircleStatus
-  from './components/CircleStatus/CircleStatus';
-import TravisStatus
-  from './components/TravisStatus/TravisStatus';
-import './App.css';
+import { RefreshIntervals } from "./Constants";
+import React from "react";
+import CircleStatus from "./components/CircleStatus/CircleStatus";
+import TravisStatus from "./components/TravisStatus/TravisStatus";
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      secondsUntilNextReload: RefreshIntervals.PAGE / 1000,
+      secondsUntilNextReload: RefreshIntervals.PAGE / 1000
     };
   }
 
   componentDidMount() {
     setTimeout(
-      'window.location.href=window.location.href;',
+      "window.location.href=window.location.href;",
       RefreshIntervals.PAGE
     );
 
@@ -30,32 +28,30 @@ class App extends React.Component {
 
   tick() {
     this.setState(prevState => ({
-      secondsUntilNextReload: prevState.secondsUntilNextReload -
-        1,
+      secondsUntilNextReload: prevState.secondsUntilNextReload - 1
     }));
   }
 
   render() {
+    const circle = process.env.REACT_APP_ENABLE_CIRCLE ? (
+      <CircleStatus />
+    ) : null;
+    const travis = process.env.REACT_APP_ENABLE_TRAVIS ? (
+      <TravisStatus />
+    ) : null;
     return (
       <div className="App">
-        <CircleStatus />
-        <TravisStatus />
+        {circle}
+        {travis}
         <br />
         <small>
-          Build status is updated every
-          {' '}
-          {RefreshIntervals.STATUS / 1000}
-          {' '}
+          Build status is updated every {RefreshIntervals.STATUS / 1000}{" "}
           seconds.
         </small>
         <br />
         <small>
-          Forcing page reload in
-          {' '}
-          {this.state.secondsUntilNextReload}
-          {' '}
-          second
-          {this.state.secondsUntilNextReload > 1 ? 's' : ''}
+          Forcing page reload in {this.state.secondsUntilNextReload} second
+          {this.state.secondsUntilNextReload > 1 ? "s" : ""}
           .
         </small>
       </div>
